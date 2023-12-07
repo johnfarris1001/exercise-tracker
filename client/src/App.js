28742eba760303509f74b79f934ba960b3243135
John <johnfarris217@gmail.com>
2023-12-06 22:25:56 -0600

    check for logged in user upon app start

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import NavBar from "./components/NavBar";
-import { Outlet } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { UserContext } from "./contexts/UserContext";
 
 function App() {
+    const { user, setUser } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        fetch("/me").then((resp) => {
+            if (resp.ok) {
+                resp.json().then((user) => setUser(user));
+            }
+        });
+    }, [setUser]);
+
     const onLogin = (user) => {
         setUser(user);
         navigate("/");
@@ -12,6 +25,11 @@ function App() {
             <br />
             <NavBar />
             <br />
+            <h4>
+                {user
+                    ? `You are Logged in as:  ${user.username}`
+                    : `You are not Logged in`}
+            </h4>
             <Outlet context={onLogin} />
         </div>
     );
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { UserProvider } from "./contexts/UserContext";
 import "./index.css";
 import routes from "./routes";
 
 const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+root.render(
+    <UserProvider>
+        <RouterProvider router={router} />
+    </UserProvider>
+);
