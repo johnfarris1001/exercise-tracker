4dec9751c899aa3f99b9e921acb051b9f222e519
John <johnfarris217@gmail.com>
2023-12-31 11:21:17 -0600

    add user and profile seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Users
+User.create(username: 'firstSeedUser', password_digest: BCrypt::Password.create('password'))
+User.create(username: 'secondSeedUser', password_digest: BCrypt::Password.create('password'))
+
+# Profile 
+user = User.first
+Profile.create(name: Faker::Name.name, bio: Faker::Lorem.paragraph, height: 70, weight: 200, user: user)
+
+# Instructors
+
+# Locations
+
+# Activities
