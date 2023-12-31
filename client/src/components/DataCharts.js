d39f4fdb8e82d0e726b162a8bec572692f8349b7
John <johnfarris217@gmail.com>
2023-12-31 00:53:01 -0600

    add dropdown to data charts

--- client/src/components/DataCharts.js
+++ client/src/components/DataCharts.js
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import { Dropdown } from "semantic-ui-react";
+import weekOptions from "../weeks";
+
 function DataCharts() {
-    return <h1>Data Charts</h1>;
+    const [weeksAgo, setWeeksAgo] = useState(-1);
+
+    return (
+        <div>
+            <h1>Data Charts</h1>
+            <Dropdown
+                selection
+                search
+                options={weekOptions}
+                value={weeksAgo}
+                onChange={(e, { value }) => setWeeksAgo(value)}
+            />
+        </div>
+    );
 }
 
 export default DataCharts;
