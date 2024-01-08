5c91210efd2053f4ac877c329fc7f1c1de4f998e
John <johnfarris217@gmail.com>
2024-01-08 09:25:42 -0600

    destroy activities on destroy user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     validates :username, presence: true, uniqueness: true
 
-    has_many :activities
+    has_many :activities, dependent: :destroy
     has_many :instructors, through: :activities
     has_many :locations, through: :activities
     has_one :profile, dependent: :destroy
