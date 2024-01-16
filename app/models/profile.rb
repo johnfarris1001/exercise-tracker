bfa4337d33fc8462e45ceb908dfa6881440ac4ec
John <johnfarris217@gmail.com>
2024-01-16 09:17:51 -0600

    tighten up profile validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,8 @@
 class Profile < ApplicationRecord
   validates :name, presence: true
-  validates :height, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than: 100 }
-  validates :weight, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :bio, presence: true, length: { maximum: 500 }
+  validates :height, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 36, less_than: 100 }
+  validates :weight, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 50, less_than: 1000 }
+  validates :bio, presence: true, length: { minimum: 25, maximum: 500 }
 
   belongs_to :user
 end
